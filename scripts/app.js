e50a853b230e7794e56f3660ac001107101a55d1
Rory Swietlicki <rory@swietlicki.eu>
2025-03-28 14:34:20 +0000

    Mastermind

---
+++ scripts/app.js
@@ -0,0 +1,54 @@
+// * When I load the page I should know what I'm playing
+
+/* Have a "start game" button 
+    - clicking "start game" will tell the computer to get its colour combanation
+    - The "start game" button will 'hide'
+    - The "submit" button will appear
+    - The player will be able to select colours
+
+/* The computer will select a colour combanation of 4 colours from a 6 colour range
+    - The colours will be selected using an array and I will use math.random for the randomization
+
+/* I can select from an option of 6 colours from the colour panel to input
+    - A button will be used for the selection
+*/
+
+/* Once a colour is picked I can choose to make 1 of 4 squares(holes) the selected colour
+    - This will respond only after a colour has been chosen from the colour panel 
+    - I can do this through selecting the square(hole) in the form of a button
+*/
+
+/* Once all 4 squares(holes) have been filled with a colour I can submit my guess
+    - I will store the selected colours in an array and compare it to the computers chosen to determine the result
+    - The 'submit' button can only be pressed once all 4 squares(holes) have been filled with a colour
+*/
+
+/* Afetr sumbmitting, depending on the player choice of colours, they will see a white or black peg
+    - From the result comparison I will display no pegs, or white or blakc pegs
+    - White: One or more colours match the computers choice
+    - Black: one or more colours match the compiters choice and the position
+
+
+/* Then repeat the process (max of 10 guesses) on the next row until you guess the colour combination or loose
+    - If guessed correctly display a "you win" screen with audio, and a promt to play again
+    - If guessed incorretly, afetr all 10 guesses, display a "you loose" screen with audio and the computers chosen colour combanation, and a promt to play again
+    - if "play again" is pressed the computer will begin the randomization process and the game will start again
+*/
+
+
+// !! Elements::
+
+// start game button
+// submit button
+// individual colour buttons
+// button for each panel on board grid
+
+
+
+// !! Executions::
+
+
+
+// !! Events::
+
+// 
