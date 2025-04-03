61bec3b329710ba6b2f226f04cf58962197fb143
Rory Swietlicki <rory@swietlicki.eu>
2025-04-03 23:18:29 +0100

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,22 @@
 // * When I load the page I should know what I'm playing
 
-/* Have a "start game" button 
-    - clicking "start game" will tell the computer to get its colour combanation
-    - The "start game" button will 'hide'
-    - The "submit" button will appear
-    - The player will be able to select colours
 
-/* The computer will select a colour combanation of 4 colours from a 6 colour range
-    - The colours will be selected using an array and I will use math.random for the randomization
-
-/* I can select from an option of 6 colours from the colour panel to input
-    - A button will be used for the selection
-*/
-
-/* Once a colour is picked I can choose to make 1 of 4 squares(holes) the selected colour
-    - This will respond only after a colour has been chosen from the colour panel 
-    - I can do this through selecting the square(hole) in the form of a button
-*/
-
-/* Once all 4 squares(holes) have been filled with a colour I can submit my guess
-    - I will store the selected colours in an array and compare it to the computers chosen to determine the result
-    - The 'submit' button can only be pressed once all 4 squares(holes) have been filled with a colour
-*/
-
-/* Afetr sumbmitting, depending on the player choice of colours, they will see a white or red peg
-    - From the result comparison I will display no pegs, or white or Red pegs
-    - White: One or more colours match the computers choice
-    - Red: one or more colours match the computers choice and the position
-
-
-/* Then repeat the process (max of 10 guesses) on the next row until you guess the colour combination or loose
-    - If guessed correctly display a "you win" screen with audio, and a promt to play again
-    - If guessed incorretly, afetr all 10 guesses, display a "you loose" screen with audio and the computers chosen colour combanation, and a promt to play again
-    - if "play again" is pressed the computer will begin the randomization process and the game will start again
-*/
-
-
+const allPegs = document.querySelectorAll('.pegs')
 const submit = document.querySelector('#submit')
 const boardButtons = document.querySelectorAll('.board-button')
 const currentColorBox = document.querySelector('#current-color')
 const colorPanel = document.querySelectorAll('.color-button')
 
 let currentRowColors = [
+    '',
+    '',
+    '',
+    '',
+]
+
+let pegArray = [] // filled with red or white
+
+const matches = [
     '',
     '',
     '',
@@ -56,7 +31,7 @@ const allColors = ['blue', 'red', 'orange', 'yellow', 'pink', 'green']
 
 // * Computer Code
 
-const computerColorCode = [
+let computerColorCode = [
     allColors[Math.floor(Math.random() * 6)],
     allColors[Math.floor(Math.random() * 6)],
     allColors[Math.floor(Math.random() * 6)],
@@ -98,43 +73,159 @@ boardButtons.forEach(btn => {
 })
 
 
+// * Check matches
 
-// * Row change, peg display and code comparrison
+// * Check[0]
 
-function submitFunctions() {
+function checkMatches0() {
 
-    if (computerColorCode[0] === currentRowColors[0] &&
-        computerColorCode[1] === currentRowColors[1] &&
-        computerColorCode[2] === currentRowColors[2] &&
-        computerColorCode[3] === currentRowColors[3]
-    ) {
-        console.log('You won!')
+    if (currentRowColors[0] === computerColorCode[0]) {
+        matches[0] = currentRowColors[0];
+        console.log(matches)
     }
-
-    else if (computerColorCode[0] === currentRowColors[0] ||
-        computerColorCode[1] === currentRowColors[1] ||
-        computerColorCode[2] === currentRowColors[2] ||
-        computerColorCode[3] === currentRowColors[3] &&
-        dataset.pegRow === currentPegRow
-    ) {
-        dataset.pegRow.classList.add('red')
+    else if (!matches[0]) {
+        currentRowColors[0] === computerColorCode[0] ||
+            currentRowColors[0] === computerColorCode[1] ||
+            currentRowColors[0] === computerColorCode[2] ||
+            currentRowColors[0] === computerColorCode[3],
+            console.log('display white')
     }
-
-    else if (computerColorCode.includes(currentRowColors[0]) ||
-            computerColorCode.includes(currentRowColors[1]) ||
-            computerColorCode.includes(currentRowColors[2]) ||
-            computerColorCode.includes(currentRowColors[3]) &&
-            dataset.pegRow === currentPegRow
-    ) {
-        dataset.pegRow.classList.add('white')
-    }
-
-    else {
-        console.log('no')
-    }
-    // currentPegRow -=1
-    currentRow -= 1
 }
 
+submit.addEventListener('click', checkMatches0)
 
-submit.addEventListener('click', submitFunctions)
+// * Check[1]
+
+function checkMatches1() {
+
+    if (currentRowColors[1] === computerColorCode[1]) {
+        matches[1] = currentRowColors[1],
+            console.log(matches)
+    }
+    else if (!matches[1]) {
+        currentRowColors[1] === computerColorCode[0] ||
+            currentRowColors[1] === computerColorCode[1] ||
+            currentRowColors[1] === computerColorCode[2] ||
+            currentRowColors[1] === computerColorCode[3],
+            console.log('display white')
+    }
+}
+
+submit.addEventListener('click', checkMatches1)
+
+// * Check[2]
+
+function checkMatches2() {
+
+    if (currentRowColors[2] === computerColorCode[2]) {
+        matches[2] = currentRowColors[2],
+            console.log(matches)
+    }
+    else if (!matches[2]) {
+        currentRowColors[2] === computerColorCode[0] ||
+            currentRowColors[2] === computerColorCode[1] ||
+            currentRowColors[2] === computerColorCode[2] ||
+            currentRowColors[2] === computerColorCode[3],
+            console.log('display white')
+    }
+}
+
+submit.addEventListener('click', checkMatches2)
+
+// * Check[3]
+
+function checkMatches3() {
+
+    if (currentRowColors[3] === computerColorCode[3]) {
+        matches[3] = currentRowColors[3],
+            console.log(matches)
+    }
+    else if (!matches[3]) {
+        currentRowColors[3] === computerColorCode[0] ||
+            currentRowColors[3] === computerColorCode[1] ||
+            currentRowColors[3] === computerColorCode[2] ||
+            currentRowColors[3] === computerColorCode[3],
+            console.log('display white')
+    }
+}
+
+submit.addEventListener('click', checkMatches3)
+
+// * Check win or loose
+
+const youWon = document.getElementById('you-won')
+const playAgain = document.getElementById('play-again')
+const youLoose = document.getElementById('you-lost')
+
+function checkWin() {
+    if (matches[0] === computerColorCode[0] &&
+        matches[1] === computerColorCode[1] &&
+        matches[2] === computerColorCode[2] &&
+        matches[3] === computerColorCode[3]) {
+        youWon.style.visibility = "visible"
+        playAgain.style.visibility = "visible"
+        submit.style.visibility = "hidden"
+    }
+    else if (currentRow === 1) {
+        youLoose.style.visibility = "visible"
+        playAgain.style.visibility = "visible"
+        submit.style.visibility = "hidden"
+    }
+}
+
+submit.addEventListener('click', checkWin)
+
+// * Peg index moover
+
+const currentPegEl = document.querySelectorAll(`[dataset-pegRows]`)
+
+function movePegIndex() {
+
+if (Number(currentPegEl) === currentPegRow) {
+
+    for (let i = 0; i < pegArray.length; i++) {
+        currentPegEl[0].classList.add(pegArray[i])
+    }
+}
+}
+
+submit.addEventListener('click', movePegIndex)
+
+
+// * Change row
+
+function changeRow() {
+    currentRow -= 1
+    currentPegRow -= 1
+}
+
+submit.addEventListener('click', changeRow)
+
+// * Play Again
+
+function clickAgain() {
+
+    currentRow = 10
+    currentPegRow = 10
+
+    while (computerColorCode.length > 0) {
+        computerColorCode.pop()
+    }
+    computerColorCode = [
+        allColors[Math.floor(Math.random() * 6)],
+        allColors[Math.floor(Math.random() * 6)],
+        allColors[Math.floor(Math.random() * 6)],
+        allColors[Math.floor(Math.random() * 6)],
+    ]
+    console.log(computerColorCode)
+
+    // boardButtons.classList.add('.white')
+    // allPegs.classList.add('.black')
+
+    submit.style.visibility = "visible"
+    youWon.style.visibility = "hidden"
+    playAgain.style.visibility = "hidden"
+    youLoose.style.visibility = "hidden"
+}
+
+playAgain.addEventListener('click', clickAgain)
